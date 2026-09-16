89ba4caa4b74a7cec8d0d2a7889e5b4e8a93f96b
Shivam Arora <37779038+arshiv-web@users.noreply.github.com>
2026-09-16 15:53:14 -0400

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int find(vector<int> &nums, int i) {
+        vector<int> dp(nums.size()-1);
+        dp[0] = nums[i];
+        dp[1] = max(nums[i], nums[i+1]);
+
+        for(int k = 2; k < nums.size() - 1; k++) {
+            dp[k] = max(dp[k-1], dp[k-2] + nums[i+2]);
+            i++;
+        }
+        return dp[nums.size()-2];
+    }
+
+
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0], nums[1]);
+        return max(find(nums, 1), find(nums, 0));
+    }
+};
