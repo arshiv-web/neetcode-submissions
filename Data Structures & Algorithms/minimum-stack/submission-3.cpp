dc3e8b1d9601a70d5137ce2f4679dcd86fd0eeeb
Shivam Arora <37779038+arshiv-web@users.noreply.github.com>
2026-09-17 02:55:59 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> ms;
+public:
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(ms.empty() || val <= ms.top()){
+            ms.push(val);
+        }
+    }
+    
+    void pop() {
+        int x = st.top();
+        st.pop();
+        if(!ms.empty() && x == ms.top()){
+            ms.pop();
+        }
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return ms.top();
+    }
+};
