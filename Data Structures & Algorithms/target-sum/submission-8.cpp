54d89d9796c7b74ee9b4bba9c37b69797b1db7f8
Shivam Arora <37779038+arshiv-web@users.noreply.github.com>
2026-09-20 19:50:59 -0400

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int run(vector<int> &nums, int target, int idx){
+        if(idx == nums.size()){
+            return target == 0;
+        }
+        return run(nums, target - nums[idx], idx + 1) + run(nums, target + nums[idx], idx + 1);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        vector<unordered_map<int, int>> dp(nums.size());
+        int maxa = accumulate(nums.begin(), nums.end(), 0);
+        int mina = -maxa;
+        dp[nums.size() - 1][nums[nums.size() - 1]]++;
+        dp[nums.size() - 1][-nums[nums.size() - 1]]++;
+        for(int i = nums.size() - 2; i >=0; i--){
+            for(int j = mina; j <= maxa; j++){
+                dp[i][j] = dp[i+1][j - nums[i]] + dp[i+1][j + nums[i]];
+            }
+        }
+        return dp[0][target];
+    }
+};
